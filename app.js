759c29d1ecd76207d2d479ff8aea24d8cd9203e7
llangis <langis85@outlook.com>
2024-01-23 08:50:12 -0400

    Finished week2

--- app.js
+++ app.js
@@ -30,4 +30,49 @@
    console.log(l);
    console.log(a);
 
-   
+   /* Author: Luc Langis
+   Title: Working with strings
+   Date: 2024-01-23*/
+
+   console.log(l);
+
+   let b = 'abcdefg';
+
+   console.log(b);
+
+   b = b.toUpperCase();
+
+   console.log(b);
+
+   b = b.toLowerCase();
+
+
+   console.log(l);
+   //substr
+   //abcdefg
+   //0123456
+
+   let c = b.substr(2,4);
+   console.log(c);
+   console.log(b);
+
+   //substring
+   //abcdefg
+   //o123456
+
+   let d = b.substring(2,6);
+   console.log(d);
+
+   console.log(l);
+
+   let hisName = 'Christopher';
+
+   let index = hisName.indexOf('ist');
+
+   console.log(index);
+
+
+
+
+
+
